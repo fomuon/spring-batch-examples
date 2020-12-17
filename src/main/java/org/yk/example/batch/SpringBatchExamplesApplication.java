129197dd92e97cb95ac7556900b8013792267b77
이용규 <yongkyu.lee@navercorp.com>
2020-12-18 00:34:09 +0900

    add func2

--- src/main/java/org/yk/example/batch/SpringBatchExamplesApplication.java
+++ src/main/java/org/yk/example/batch/SpringBatchExamplesApplication.java
@@ -10,5 +10,6 @@ public class SpringBatchExamplesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBatchExamplesApplication.class, args);
+		System.out.println("func2");
 	}
 }
